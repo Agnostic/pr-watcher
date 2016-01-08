a7fa010378980d84109b8d621921c36a58501a0e
Gilberto Avalos <gilberto@ooyala.com>
2016-01-08 13:06:35 -0600

    Add Github service

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 var app = angular.module('app', []);
 
+app.service('Github', [function() {
+  var baseUrl = 'http://github.services.ooyala.net';
+  var token = localStorage.getItem('accessToken');
+
+  return {
+  };
+}]);
+
 app.run(['$http', function($http) {
   var clientId = 'fcc57884b81d98d9bee3';
   var baseUrl = 'http://github.services.ooyala.net';
@@ -14,7 +22,11 @@ app.run(['$http', function($http) {
         client_secret: '1a9367f8ab0ed7eec4db37d464c25c55a5f741c5',
         code: code
       }).then(function(response) {
-        console.log(response.data);
+        if (response.data.access_token) {
+          localStorage.setItem('accessToken');
+        } else {
+          location.href = '/';
+        }
       });
   } else {
     location.href = baseUrl + '/login/oauth/authorize?client_id=' + clientId;
