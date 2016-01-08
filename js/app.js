0fefa1df750d3e85fae3f6f01d48b89013824bc5
Gilberto Avalos <gilberto@ooyala.com>
2016-01-08 12:56:52 -0600

    Add oauth login

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 var app = angular.module('app', []);
 
 app.run(['$http', function($http) {
-  location.href = 'http://github.services.ooyala.net/login/oauth/authorize?client_id=fcc57884b81d98d9bee3';
+  var code;
+
+  if (location.search.match(/code/)) {
+    code = location.search.substr(1, location.search.length).split('=')[1];
+  } else {
+    location.href = 'http://github.services.ooyala.net/login/oauth/authorize?client_id=fcc57884b81d98d9bee3';
+  }
   console.log('Run', location.search);
 }]);
