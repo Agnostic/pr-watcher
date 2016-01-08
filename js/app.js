194803fa1a48b2265e9b828088c62b6fb4798bc2
Gilberto Avalos <gilberto@ooyala.com>
2016-01-08 13:00:54 -0600

    Add request to get access_token

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 var app = angular.module('app', []);
 
 app.run(['$http', function($http) {
+  var clientId = 'fcc57884b81d98d9bee3';
+  var baseUrl = 'http://github.services.ooyala.net';
   var code;
 
   if (location.search.match(/code/)) {
     code = location.search.substr(1, location.search.length).split('=')[1];
+
+    $http
+      .post(baseUrl + '/login/oauth/access_token', {
+        client_id: clientId,
+        client_secret: '1a9367f8ab0ed7eec4db37d464c25c55a5f741c5',
+        code: code
+      }).then(function(response) {
+        console.log(response.data);
+      });
   } else {
-    location.href = 'http://github.services.ooyala.net/login/oauth/authorize?client_id=fcc57884b81d98d9bee3';
+    location.href = baseUrl + '/login/oauth/authorize?client_id=' + clientId;
   }
   console.log('Run', location.search);
 }]);
