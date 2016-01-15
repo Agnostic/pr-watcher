b965204b8fb0c6ed3378abc7bcfc781d426664e5
Gilberto Avalos <gilberto@ooyala.com>
2016-01-15 10:12:44 -0600

    Filters

--- js/app.js
+++ js/app.js
@@ -98,10 +98,9 @@ app.controller('reposController', ['$scope', 'Github', '$http', '$timeout', func
     return body.match(user);
   };
 
-  // $scope.showRepo = function(id) {
-  //   console.log($('#repo_' + id).find('tr:not(.ng-hide)'));
-  //   return $('#repo_' + id).find('tr:not(.ng-hide)').length;
-  // };
+  $scope.hideRepo = function(id) {
+    return !$('#repo_' + id).find('tr').length;
+  };
 
   // $scope.showOrg = function(id) {
   //   return $('#org_' + id).find('.repository:visible').length;
